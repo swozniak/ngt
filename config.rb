6bf25ed6926c22a739d6a8ac83f0c7b19b8f49bc
Steve Wozniak <steve@swozniak.local>
2015-10-02 20:41:53 -0400

    checking in progress

---
+++ config.rb
@@ -0,0 +1,15 @@
+# Require any additional compass plugins here.
+require "susy"
+require "companimation"
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "/"
+sass_dir = "sass"
+images_dir = "images"
+images_dir = "images"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+
+relative_assets = true
+output_style = :compressed
---
+++ index.php
@@ -0,0 +1,176 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<title>Never Get Tired: The Bomb the Music Industry! Story</title>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="A documentary film about Bomb the Music Industry!" />
+
+	<link rel="stylesheet" type="text/css" href="main_style.css" media="all" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+	<header>
+		<section class="ngt-header">
+			<div class="nff-logo">
+				<img src="images/nf-logo.png" alt="No Future Films" title="No Future Films" />
+			</div>
+			<ul class="ngt-social">
+				<li class="ngt-menu-item"><a target="_blank" href="https://vimeo.com/saracrow/videos"><i class="fa fa-vimeo-square"></i></a></li>
+				<li class="ngt-menu-item"><a target="_blank" href="https://twitter.com/NGTfilm"><i class="fa fa-twitter-square"></i></a></li>
+				<li class="ngt-menu-item"><a target="_blank" href="https://www.facebook.com/nevergettiredmovie"><i class="fa fa-facebook-official"></i></a></li>
+				<li class="ngt-menu-item"><a target="_blank" href="mailto:sara@nofuturefilms.com"><i class="fa fa-envelope-o"></i></a></li>
+			</ul>
+		</div>
+	</header>
+
+	<div class="ngt-container">
+		<section class="ngt-splash">
+			<div class="ngt-title">
+				<img src="images/title-never.png" alt="Never" title="Never" />
+				<img src="images/title-get.png" alt="Get" title="Get" />
+				<img src="images/title-tired.png" alt="Tired" title="Tired" />
+			</div>
+			
+			<div class="ngt-trailer">
+				<div class="ngt-trailer-embed-container">
+					<iframe src="https://www.youtube.com/embed/W3Oz-dmCxxs??VQ=HD720" frameborder="0" allowfullscreen></iframe>
+				</div>
+			</div>
+
+			<div class="ngt-subhead-container">
+				<img class="img-responsive" src="images/the-btmi-story.png" alt="The Bomb the Music Industry! Story" title="The Bomb the Music Industry! Story" />
+				<img class="img-responsive" src="images/credits.png" alt="NO FUTURE FILMS Presents a Film by Sara Crow" title="NO FUTURE FILMS Presents a Film by Sara Crow" />
+			</div>
+		</section>
+
+		<section class="ngt-screenings">
+			<div class="ngt-max-width">
+				<div class="ngt-h2-container">
+					<h2>Upcoming Screenings</h2>
+				</div>
+				
+				<article class="ngt-screening">
+					<strong>10/7: Los Angeles, CA. SideOneDummy. 7:30pm.</strong><br />
+					Q+A with filmmakers after the screening.</br>
+					Must RSVP! BYO Chair! Suggested donation $5.<br />
+					<a target="_blank" href="https://www.facebook.com/events/1476686852633066/">RSVP &amp; Event Info</a>
+				</article>
+
+				<article class="ngt-screening">
+					<strong>10/31: Gainesville, FL. The Hippodrome Theater. 12:45pm.</strong><br />
+					Tickets are $10.<br />
+					<a target="_blank" href="http://nevergettiredfest.brownpapertickets.com/">Tickets</a>
+				</article>
+
+				<article class="ngt-screening">
+					<strong>11/30: Chicago, IL. Beat Kitchen. 7pm.</strong><br />
+					Q+A with filmmakers after the screening.</br>
+					Tickets are $10.<br />
+					<a target="_blank" href="https://www.ticketfly.com/purchase/event/968261">Tickets</a> / <a target="_blank" href="https://www.facebook.com/events/1476686852633066/">Event Info</a>
+				</article>
+
+				<article class="ngt-screening">
+					<strong>12/7: Rochester, NY. The Cinema Theater. 6:30pm.</strong><br />
+					Presale tickets are $8.<br />
+					<a target="_blank" href="http://www.brownpapertickets.com/event/2327677">Tickets</a> / <a target="_blank" href="https://www.facebook.com/events/451174275090033/">Event Info</a>	
+				</article>
+			</div>
+		</section>
+
+		<section class="ngt-story">
+			<div class="ngt-max-width">
+				<div class="ngt-h2-container">
+					<h2>Never Get Tired: The Story</h2>
+				</div>
+
+			    <div class="ngt-story-p-container">
+			    	<p>At a moment when crowdfunding seems like it will be the future of the industry, <strong>NEVER GET TIRED</strong> takes as its focus the world&rsquo;s first donation&ndash;based band, <strong>BOMB THE MUSIC INDUSTRY!</strong> and its variously charismatic, depressive, big&ndash;hearted, and loudmouthed mastermind, <strong>JEFF ROSENSTOCK</strong>. </p>
+				</div>
+
+				<div class="ngt-story-p-container">
+					<p>Jeff takes seriously his complex relationship to commerce, community and diy ethics: all the music is free, all shows are all ages and under $10, there is no merchandise. These practices, coupled with Jeff&rsquo;s relentlessly positive us&ndash;versus&ndash;them mentality, has earned him the most ardently committed fan base in the punk scene. Fans around the world cling to Jeff as the only true punk icon of their generation, but as Jeff rounds out a decade with <strong>BOMB THE MUSIC INDUSTRY!</strong> and begins to outgrow the chaos, he is confronted with the disparate demands of his fans, his loved ones, and his ideals. <strong>NEVER GET TIRED</strong> tells the story of what happens when the weight of the world outside threatens to break the man, challenging him to choose between his career and his life, his fans and his loved ones, a transient lifestyle and financial stability.</p>
+				</div>
+
+				<div class="ngt-story-p-container">
+					<p>Years before Radiohead and Nine Inch Nails offered free downloads of their new releases, Rosenstock founded the first-ever donation-based label, <strong>QUOTE UNQUOTE RECORDS</strong>. In response to the collapse of his more industry&ndash;minded former band, Jeff launched the label and <strong>BOMB THE MUSIC INDUSTRY!</strong> from home with just a guitar and his computer.</p>
+				</div>
+
+				<div class="ngt-story-p-container">
+					<p>The music sparked something in basements and DIY venues worldwide. Jeff's donation&ndash;based model worked in a swollen music industry that needed saving. The pay&ndash;what&ndash;you&ndash;can model worked because it was inclusive, creating an interdependency between the musician and the fan. People donated, and for the last ten years <strong>BOMB THE MUSIC INDUSTRY!</strong> has headlined sold&ndash;out music festivals, played packed houses worldwide, played on Showtime and NBC, and sold tens of thousands of records in spite of offering them for free online.</p>
+				</div>
+
+				<div class="ngt-story-p-container">
+					<p>And <strong>NEVER GET TIRED</strong> was along for the ride. Five years, eleven countries, and three hundred and fifty hours of footage later and the film is shot. Thanks to Kickstarter, back in 2010 I made enough money to buy a camera and go on one tour. The intervening four years was nothing short of a financial miracle. Maxed out credit cards, my personal savings, the generosity of BTMI!, and the volunteered time of an amazingly talented crew allowed me to finish shooting <strong>NEVER GET TIRED</strong> with no significant film funding.<br /><br />In May of 2014, <strong>NEVER GET TIRED</strong> exceeded its fundraising goal on Kickstarter and premiered on July 24, 2015 in New York City.</p>
+				</div>
+			</div>
+		</section>
+
+		<section class="ngt-contact">
+			<div class="ngt-max-width">
+				<div class="ngt-h2-container">
+					<h2>Contact</h2>
+				</div>
+
+				<div class="ngt-contact-me">EMAIL ME! sara [at] nofuturefilms [dot] com.</div>
+			</div>
+		</section>
+
+		<section class="ngt-faq">
+			<div class="ngt-max-width">
+				<div class="ngt-h2-container">
+					<h2>F.A.Q.</h2>
+				</div>
+				
+				<div class="ngt-faq-question">When can I download the movie?</div>
+				<div class="ngt-faq-answer">There are some expensive legal fees that come with distributing your film online. I&rsquo;m an independent filmmaker and I work full&ndash;time, so I&rsquo;m saving up money and inching towards being able to do that. I anticipate downloads will be available by the end of the year, but until then we are focusing on screening the film in theaters throughout 2015.</div>
+				
+				<div class="ngt-faq-question">Can I set up a screening in my town?</div>
+				<div class="ngt-faq-answer">Totally! We have set up screenings in Berlin, Arizona, Florida, Texas and Los Angeles. If you book stuff in your town and want to set something up, e-mail me.</div>
+				
+				<div class="ngt-faq-question">Where is my backer reward?</div>
+				<div class="ngt-faq-answer">We sent out all the backer rewards at the end of 2014, and we sent several messages to the backers asking for correct addresses/sizes etc. If you didn&rsquo;t get your reward, I apologize, but we tried our best to get a response from you and the odds are you either moved or didn&rsquo;t answer the survey. Digital downloads will go out by the end of the year.</div>
+				
+				<div class="ngt-faq-question">Do you have extra rewards from the Kickstarter?</div>
+				<div class="ngt-faq-answer">We do still have some of Mitch Clem&rsquo;s posters. If you are interested in buying one e&ndash;mail me. There are no more stickers or t-shirts.</div>
+			</div>
+		</section>
+
+		<section class="ngt-backers">
+			<div class="ngt-max-width">
+			<div class="ngt-backers-content-container">
+				<div class="ngt-h2-container">
+					<h2>Kickstarter Supporters</h2>
+				</div>
+
+				<aside class="ngt-poster-container">
+					<img class="img-responsive ngt-poster" src="images/poster.jpg" align="left" />
+				</aside>
+
+				<div class="ngt-names">Cristina Barros, Jill Adelphia, Will Feinstein, Dylan Bargteil, Frank Farrell, Jeremy Logan, John DeDomenici, Jesse Denton, Samuel L Sweitzer, Chris Ahearn, Josh Elliott, Daniel Robinson, Robbie Colvin, Josh LaPlant, Audrey Farwagi, Diana Costello, Alex Reeves, De Wright, Rachel Clark, Nick Harvey, Kyle Dreiling, Carlo Cavaluzzi, Decker Cavosie, Jamie Wettlaufer, Leo Harvey, Marc Zingarini, Wes Harbison, Joachim W. Walewski, Lewis Winter, Sean Bradley, Alex Lowry, Colin Castor, Emily Ann Rinaldi, Pete D'Angelo, Graham Taylor, Kegan Ruthsatz, Joey Casamento, Adam D., Lukas Myhan, Brendan Cain, Chris Perez, Nick Bethune, Nick Vannucci, Eric Greene, Steven W. Allen, Joshua Muller, Stefan Salamone, Jeff Klepper, Nicholas Moryl, Bridget MacFarland, Jay Adams, Michael Eidlin, Shawn Levy, Ryan Triston Case, Ely Dorantes, Nick Story, Jason Siebert, Max Timper, Dan Macor, Megan M. Headley, Steven P. Lewis, Pawel Gabryelski, Ellen Keating, Eric Schmidt, Isaac Mendoza, Jeff Zeitler, Dwight Upton, Megan Frederick, Conor Burke, Michael Goh, Ian Rousso, Rinat, Harvey, Elizabeth Day, Rye Barbour, Brian O'keefe, Andrew Michael Potter, Alex Alverson, Eric Gaines, Patrick Stalcup, RJ Yancey, Joe Vaas, Dan Kinder, Aaron Katz, Brendan Killion, Julie Taylor, Dillon Minacci, CJ Wiemer, Mike McGowan, Colleen Napolitano, Randal Bernhoft, Jordan Donnelly, Mark Edsall, Joshua Cappell, Johnny Gonzalez, Alex Lomelino, Jackson Watkins, Chris Grunwaldt, Gareth Case, Danny Revenant, Ryan Hupperts, Andrew Heaton, JD Watson, Joan Kim, Derrick Uchida, Kurt Haeusser, Allie Kerr, Nate Adams, Shea Kissell, Karlis Lauva, Eric Knepper, AJ Valenzuela, jonathon spagenske, Mike Lema, Erik Bubeck, Jake Brand, Daniel De Andrade, Timofei Shatrov, Jake Margo, Adam Gentle, Joshua Poole, Chad Dudley, John David Bujalski, Lori Wilson, Julian Mercuri, Will McNamara, Rachel Bell, Greg Stevenson, Erika Refsland, Jon Moore, Kahai Furugen, Brad Cornforth, Jonathan Daily, Nicholas Hubler, Christopher DeCerbo, Jeremy Elias, Adriana Vaccaro, Jesse Medina, Maribel Ledezma-Williams, Darren Ziegler, Reid Smith, Grant Granado, John Shields, Donald Goldstein, Mara Ginsberg, Jeffrey Pohl, Lane Avenue, Liz Roswell, Joe Pow, Stephanie Tougas, Michael Maiello, Bret Bouchard, Tommy Gaston,  John Komar, Al Nelson, David Siffert, Sam Logan, Steve Wozniak, Kevin Fleischman, Mike Bartholomay, Owen Harrelson, Jo Amelia Finlay, Stacy Frankel, Ben Shaw, Barbara Pohl, Sebastian Weinberg, Michael Mahaffie, Dylan Rowlands, Colin Logue, Alex Wolf, Ryan Harrington, Jesse Connor, Bryan R Wade, Alex Walker, Luke Takuya Palermo, Kat Steinmetz, Tony Henley, Kevin Van Nguyen, Katie Rubinas, Melanie Wade, Dan Sickles, James Webster, Daniel Kovar, Julia Obejero-Paz, Patrick Ander, Matty Sunshine, Nathaniel G Winter, Amanda Beneway, Aaron Wimmer, Jon Koch, Brian McNamara, Timothy Gomez, Nick Ehly, David Contino, Eddie Belaval, Chris McDonald, Matthew Ern, Lars Roeder, Ian and Sarah Jennings, Derek Rife, Craig M Winer, Alexandria Bell, Helen Blanderson, Sam Palleschia, Lori Jenkins, Nick Pico, Zach S, Nicole Beaudoin, Mel Weyant, Julien Feldman, Benjamin Ridder, Dan Mietz, NJ Smith, Liz Tomblin, Daniel Spiegelman, Kyle Petrovich, Christopher Thomas, Kyle Allen, Justin Emmerton, Bradford Lipschitz, Steve Scapicchio, Johnathan Byrd, Eddie Escobedo, Danielle DiSisto, Eugene and Kari Renne, Jake Roby, Mari Campos-Seligman, John Elster, Chris Wallace, Ana Fabrega, Bailey Bennett, Andrew Hall, Pedro Silva, Chandler Howie, Sam Hanley, Max Wilson, Jim Taone, Matthew McLaren, Vernon Mahoney, Tony Jacquin,  Alexander Freidman, Brizer St. Cyr, Matthew Yglesias, Samuel Gursky, Jami Carignan, Enzo Priesnitz, Ryan Bilgrav, Kyle H, Justine Ciullo, Daniel Speer, Cam Scheller-Suitor, Michael Edwards, Eric Dresden, Luke Colianni, David Wetmore, Michael Wagner, Kyle Weaver, Meg Stepp, Anthoni Awad, Ryan Murphy, Ray Miranda, Matt Donaher, Jeffery Puz, John McGuire, Kyde and Eric Marshall, James ikeda, Ned Wyer, Ricky Stevens, Jesse Stephens, Alexander MacMichael, James Doutney, Shane Stewart, Sara Callori, Adam Clements, Dana Hart, Matt Gallo, Ben Stewart, Melissa Burant, Michael Stafford, Aaron Dowd, Kyle Crawford, Pilot Viruet, Eric Moizuk, EJ Judge, Jenna Lapidus, Shaun Green, Christopher Butcher, Megan Whitman, Rusty Donlon, Jonathan Rollock, Scott Dektar, Tom Reynolds, Tim Ruggeri, Jared C. Ford, Alex Cassidy, Jesse Spence, Shelby Darcy, Jeremy Braun, Brian Follick, Dane Kopnisky, Ryland Roberts, Tom Belton, Cameron Brizzee, Jacob Tarnowski, Matt Puzio, Sam Skopp, Bryan Pierce, Josh Less, Matt Gaborcik, Jon Oerlemans, Gary Morin, Danny Spooner, Hayden Schoen, Colin Bober, Chaz Mcbonester, Taylor Smith, Taylor Nix, Justine Misholy, John Citzen, Elie C.Del Abraham, Connie Carbone, Ethan Marsh, Ross Case, Trevor Mattingly, Nick Esposito, Shawn and Lindsie Bird, Chad Vahosky, Evan Scheingross, Steve Kroodsma, Tyler Emery, Tim Gilman, Mike Sudano, Chris Williams , Andy Reich, Sean Mallari, Eric Brown, Cesar Cervantes, Megan Vanwyck, Same Grimes, Max V E Brooks, Dylan Da Silva, Zach Ryan Skamra, Adam Osborn, Devin Mancuso, Parker Phillips, Ronnie Leblanc, Dana Geislinger, Suzanna Shayer, Cary Young, Justin Vasko, Ellister Appleseed, Nicole Villeneuve, Dan  Buksa, Daniel Miller Kevin Brosnan, Noah Duncan, Benjamin Roach, Brad Zahn, Doug Jacklin, Max Symmes, Tommy Jarvis, Jim Golden, Connor Fanning, John Q Barnes, Ian Donohue, Pablo Orellana, Jamie Stonemetz, Atticus Stockdale, Luke Clifton-Friend, Patrinka Berry, Jake Lipp, Chris DiTommaso, Jon McCurdy, Scott Larson, Taylor Dance, Abe Dolinger, Tyler Koziar, Luke Fleishman, Connor McNerney, Sarah S. Emily Skahill, Alex Abbatiello, Kris McClendon, Matthew Lee Middleton, Devon Young, Kevin Zakszewski, Matthew Dublak, Jack Bowden, Mike Van Dorn, Max Steinbach, Chris Ressler, Mark Shimabukuro, Brian Renfrew, Tom Luca, Wesley Hampton, Stephen Bennett, Adam D'Augelli, Wyatt Lee Chastain, Chris F, Cody Paterson, Gianni Vollalobos, Joey Mohler, Kristin Marie, Richard Kelly, John Akers, Nicole Maxwell, Matthew Kirschner, Aidan Cuffe, Omar Malik, Tito Silvey Blatnik, Shamara Elizabeth, Jamie Albrecht, Christopher McClintock Lylynn Pham, Shannon Dillion, Brian Klein, Kyle Thrash, Sami Filasteen, Hailen Jackson, Adam Linder, Eric Wiemer, John Trotta, Jason Selvaggio, Matt Filyk, Ryan Pratt, Williams Gibbons, Zack Bolotin, Kurt Manuel, Matthew Bisenius, Aaron Jennings, Tori T, Harrison Bach, Dave Cassells, Derek Ackermann, Ben Fishlock, Teagan White, Richard Straub, Stuart Culpepper, Joni Malinowski, Nick Bournes, Joseph Bove, Dersh Kiersh, Eric Easton, Mike Dorval, Joel Absolom, Zachary Glanz, Rich Mandell, Joseph Aitken, E. Pollex Rasmussen, Alex Billig,  Colin Petkus, Ren Dickson, Kyle Hodgman, Christopher J Weber, Nicholas Regan, Brendan Jordan, Shawn Solis, Greg Spils, George Mauer, Lindsay BUzzo, Paulina Schwietzer, Ian Jackson, James Barke, Jon Raine, Bobby Schroeder, Neal Buzzo, Mike Bakken, Dan Smith, Danny Jarquin, Maggie Leake, Rick O. Shane, Mike DeGaetano, Jake Sposetta, Teddy Mahan, Chris Lohman, Tony Alex, Adam Boucher, Omer Shaulian, Michael Walker, Adam Marsh, Mikie Manzer, Marc Chapin, Dan Fulk, Shawn Lang, PÃ¥l Nikolaisen, George Giosi, Noah Seidel, Kevin Young, Tim Fromm, Jillian Hahnlen, Hector Arreola, Louis C Sullivan, Rich Lapham, Drew Kaufman, Christopher Swingler, Levi R., Ryan McCoy, Nick Lister, John Burroughs, Eric Miller, Shervyn Von Hoer, Alex T Vincent, Zach Hobler, Matt Long, William Lim, Spenser Chapple, Francis Stewart, Carl England, James Sailor, Chad Terpstra, Rahul Hamid, Kevin Chu, Brad Thomason Ryan Visteen, Dante Escrofani, Alvaro Bretel, Adam Tolladay, Nathan Cramer, Greg Kinneman, Chris Springfield, Nancy Roth, Elisa Lamont Zach Dierks, Stu Daly, Francesca J Cicale, Jeremy Smith, Sarah Stark, Martin Pohl, Jim Brown, Emily Brown, Duncan Cabral, David Gebhardt, Courtney Hurdle, David Fonseca, Daniel Korn, Ivan Raczycki, Lori Giovengo, Leah Belanger, David Augustad, Karen Nillion, Larrissa Jerome, Frank Babik, Kevin Davila, Mike Arcuri, Lew Frye, Bjarte Malum, Chad W Martin,  John Miskelly, Sean Rahilly, Spencer A. Taplin, Stephen Joseph Cadigan, Mike DeMatteo, Hannah Back, Michelle Duffy, Stephen Bollander, Justin Jesena, Hannah Zeitner, Ryan Whitfield, Berta Ditillo, Joan Kim,  Abby Armada, Ryan Jose Gaudet, Jason Blevins, Samantha Onorato, Kate Peltz, Kenny Scofield, William Pepple, Dee Bawcherous, Adam Gellar, Scott Jones, Cem S Korkmaz, Jake O'Donnell, Laura Crossin, John Macchia, Chris Johnert, Tom Broker, Alex Boissonnault, Andrew Taylor, Elisabeth Malone, Andy Gattis, Harvey A Sniffen, Reid Stegall, Erin and Anthony Conventry-Smith, Don Brodmerkel, Zandra Ruiz, Ricky Day,  Matt Canigiani, Craig Stasila, Thomas R Harty, Sean Kopa, Jack W Ryan, Tanawat Luekr-u-suke, Evan Lane, Holly F Stradling, Mikey Lannen, Andy Fusco, Luke Saenz, Sonora Wong, Tyler Gaw, Eddie Healey, Eric Solomon, Neal Todten, Paul Lazo, Frank Obregon, Matt Gardner, Barbara LaGrassa, Brian WakeField, Nico Fontanella, Noah Corey, Karen and Rich Stark, Trystan Rose, Kyra Swerrie, Kyle Mangione-Smith, Ariel Marshall, Jay Knobel, Erika LaGrassa, Michael Dugan</div>
+
+			</div>
+			</div>
+		</section>
+	</div>
+			
+	<footer>
+		<section class="ngt-footer">
+			<div class="ngt-footer-text-container">
+				<h6 class="ngt-footer-text">Site designed for No Future Films by <a target="_blank" href="http://twitter.com/stephanwozniak">Steve Wozniak</a>.</h6>
+			</div>
+		</section>
+	</footer>
+
+</body>
+</html>
